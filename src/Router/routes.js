46003152a0df143b35a81805a09f9206ea065c42
piagja <piagja@gmail.com>
2021-04-01 21:32:36 -0300

    Refez componente Depoimentos

--- src/Router/routes.js
+++ src/Router/routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import Dashboard from '../components/Dashboard/Dashboard'
 import Main from '../components/Main/Main'
-import Footer from '../components/Footer/Footer'
+import Depoimentos from '../components/Depoimentos/Depoimentos'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 const Routes = () => {
@@ -11,7 +11,7 @@ const Routes = () => {
       <Route path='/dashboard' component={Dashboard} />
       <Route exact path='/' component={Header} />
       <Route exact path='/' component={Main} />
-      <Route exact path='/' component={Footer} />
+      <Route exact path='/' component={Depoimentos} />
     </BrowserRouter>
   )
 }
