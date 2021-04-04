8ca331e4d92af2fff46a54799e1800e5c911b5f4
piagja <piagja@gmail.com>
2021-04-04 01:17:02 -0300

    Retirou useState da Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { AiOutlineCheck } from 'react-icons/ai'
 import CardDashboard from './CardDashboard/CardDashboard'
